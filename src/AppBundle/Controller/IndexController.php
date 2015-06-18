643d53a92f87fd0e20ad99808deaa77964f6f24a
Chris Ciampoli <chrisciampoli@gmail.com>
2015-06-17 23:48:37 -0700

    Index view and controller

---
+++ src/AppBundle/Controller/IndexController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class IndexController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     * @Template()
+     */
+    public function indexAction(Request $request)
+    {
+        return [];
+    }
+
+}
